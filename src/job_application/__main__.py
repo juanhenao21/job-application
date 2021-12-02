5fdb32a41e8fb9d86a7f6c5341c84a482faa129e
juanhenao21 <juanhenao21@gmail.com>
2021-12-02 11:20:54 +0100

    Add docstring to the main function

--- src/job_application/__main__.py
+++ src/job_application/__main__.py
@@ -5,7 +5,12 @@ import click
 @click.command()
 @click.version_option()
 def main() -> None:
-    """Job Application."""
+    """Job Application.
+
+    Python package to test the data analysis cases for a job application.
+    """
+
+    click.echo("Hello, world!")
 
 
 if __name__ == "__main__":

e5b4524dd65f468e2d2b7b9febe7c7b180121af9
jhenaolondono <henaolondono@auxmoney.com>
2021-12-23 18:46:12 +0100

    [ISSUE #2] Add the Kaggle option to the CLI

--- src/job_application/__main__.py
+++ src/job_application/__main__.py
@@ -1,15 +1,28 @@
 """Command-line interface."""
 import click
 
+from job_application import kaggle_titanic
+
 
 @click.command()
 @click.version_option()
-def main() -> None:
+@click.option(
+    "--kaggle",
+    type=bool,
+    default=False,
+    help="Boolean, True to select the Kaggle Titanic dataset.",
+)
+def main(kaggle: bool) -> None:
     """Job Application.
 
     Python package to test the data analysis cases for a job application.
+
+    Args:
+        kaggle (bool): True to select the Kaggle Titanic dataset.
     """
-    click.echo("Hello, world!")
+    if kaggle:
+        train_df = kaggle_titanic._load_train_data_kaggle_titanic()
+        print(train_df.head())
 
 
 if __name__ == "__main__":

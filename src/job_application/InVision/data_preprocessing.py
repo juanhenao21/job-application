b163642095d591aba131c096cc94aaf57320ce35
juanhenao21 <juanhenao21@gmail.com>
2022-01-16 18:03:08 +0100

    [ISSUE #3] Implement load function

--- src/job_application/InVision/data_preprocessing.py
+++ src/job_application/InVision/data_preprocessing.py
@@ -1,4 +1,16 @@
 """InVision - Data pre-processing."""
+import git
+import pandas as pd
+
+
+def _load_data() -> pd.DataFrame:
+    """Load data to analyze."""
+    repo: git.Repo = git.Repo(".", search_parent_directories=True)
+    root: str = repo.working_tree_dir
+    data: pd.DataFrame = pd.read_csv(
+        root + "/data/ads.csv", index_col=["Time"], parse_dates=["Time"]
+    )
+    return data
 
 
 def main():

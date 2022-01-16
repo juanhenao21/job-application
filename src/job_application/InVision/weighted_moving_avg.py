de380fcdc1a2eba519e291163cd55e5f2fe5b3f3
juanhenao21 <juanhenao21@gmail.com>
2022-01-16 22:29:34 +0100

    [ISSUE #3] Implement moving avg

--- src/job_application/InVision/weighted_moving_avg.py
+++ src/job_application/InVision/weighted_moving_avg.py
@@ -1,4 +1,21 @@
 """InVision - Weighted Moving Average."""
+import pandas as pd
+
+
+def moving_avg(data: pd.DataFrame, num_obs: int) -> pd.DataFrame:
+    """Computes average of the last n observations.
+
+    Args:
+        data (pd.DataFrame): series to be analyzed.
+        num_obs (int): number of observations to make the average.
+
+    Returns:
+        pd.DataFrame: dataframe with the prediction.
+    """
+    prediction = data.iloc[-num_obs:].mean()
+    data.loc[max(data.index) + pd.to_timedelta(1, unit="h")] = prediction
+
+    return data
 
 
 def main():

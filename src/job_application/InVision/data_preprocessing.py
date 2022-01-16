773ad04e4a4f4c1f69649be8b41435169575e1df
juanhenao21 <juanhenao21@gmail.com>
2022-01-16 15:35:00 +0100

    [ISSUE #3] Add InVision folder with basic files

---
+++ src/job_application/InVision/data_preprocessing.py
@@ -0,0 +1,10 @@
+"""InVision - Data pre-processing."""
+
+
+def main():
+    """Main function of the module"""
+    pass
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/job_application/InVision/weighted_moving_avg.py
@@ -0,0 +1,10 @@
+"""InVision - Weighted Moving Average."""
+
+
+def main():
+    """Main function of the module"""
+    pass
+
+
+if __name__ == "__main__":
+    main()
